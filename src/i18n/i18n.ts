d899a2100f81769411629e72a031a69800b1382b
Joonas Tiala <joonas.tiala@gmail.com>
2019-09-17 23:17:20 +0300

    Move i18n stuff to i18n folder

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import { en } from "./translations/en.json";
-import { fi } from "./translations/fi.json";
+import { en } from "./en.json";
+import { fi } from "./fi.json";
 
 i18n.use(initReactI18next).init({
   lng: "fi",
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./i18n";
+import "./i18n/i18n";
 import * as serviceWorker from "./serviceWorker";
 import App from "./components/App/App";
 
