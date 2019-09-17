acd57303e93315fff20fb619c6272288d67adc10
Joonas Tiala <joonas.tiala@gmail.com>
2019-09-17 23:18:10 +0300

    Rename defaultTheme to theme

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,7 +7,7 @@ import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
 
-import theme from "../../defaultTheme";
+import theme from "../../theme";
 import useStyles from "./App.styles";
 
 const Home: React.FC = () => {
